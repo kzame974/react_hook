0f6eba86f51feedf9599604f876a6e0f783ada96
Kzame <ludovic.surel@gmail.com>
2020-09-09 19:03:21 +0400

    add provider from redux on index

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
