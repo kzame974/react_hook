3256cee694a57940faca6c11c3cec4cef8f4c89f
Kzame <ludovic.surel@gmail.com>
2020-09-09 19:34:13 +0400

    add button and dispatch on contactJSX

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import Button from "react-bootstrap/Button";
 
 export function Contact() {
     const language = useSelector((state)=>state.language)
+    const dispatch = useDispatch();
     return (
-        <h1>{language} est sélectionné </h1>
+        <>
+            <h1>{language} est sélectionné </h1>
+            {   language === 'english' ?
+                <Button onClick={(()=>dispatch({type:"ENGLISH"}))} disabled={true}>Anglais</Button>
+                : <Button onClick={(() => dispatch({type: "ENGLISH"}))}>Anglais</Button>
+            }
+            {   language === 'french' ?
+                <Button onClick={(()=>dispatch({type:"FRENCH"}))} disabled={true}>Français</Button>
+                : <Button onClick={(() => dispatch({type: "FRENCH"}))}>Français</Button>
+            }
+        </>
     );
 }
